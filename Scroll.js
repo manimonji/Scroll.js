90590fc6c02fc5bace807d1b61763d00b40bd0e1
manimonji <69511102+manimonji@users.noreply.github.com>
2021-04-28 16:12:08 +0430

    Update Scroll.js

--- Scroll.js
+++ Scroll.js
@@ -1,7 +1,20 @@
-//Selector's
-function scrollTo(Selector,speed,callback){
-    var sel_check = Selector.match();
-    var sel_el = document.getElementById();;
-    var el_top = ;
-window
+function scrolltoelement(Selector,speed,callback){
+    var sel_id = Selector.match("#");
+    var sel_cl = "";
+    if(sel_id == ""){
+    var sel_cl = Selector.match(".");
+    }
+    var sel = sel_id + sel_cl;
+    var sel_va = Selector.replace(sel,"");
+    if (sel == ".") {
+    var sel_el = document.getElementsByClassName(sel_va);
+    }else if (sel == "#") {
+    var sel_el = document.getElementById(sel_va);
+    }else{
+    var sel_el = document.getElementsByTagName(sel_va);
+    }
+    var el_top  =  sel_el.offsetTop;
+    
+    window.scrollTo(0, el_top);
+ 
 }
